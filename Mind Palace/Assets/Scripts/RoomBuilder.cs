d1f414ef6a8dd9660529e40ae3b6acefe2003e69
SomethingKL <rbrandt@zagmail.gonzaga.edu>
2017-10-30 20:22:32 -0700

    Room Builder

---
+++ Mind Palace/Assets/Scripts/RoomBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBuilder : MonoBehaviour {
+    public GameObject floor;
+    public GameObject filledWall;
+    public GameObject doorWall;
+
+    private GameObject[] components;
+    private Vector3 roomCentre;
+
+	// Use this for initialization
+	void Start (){
+        components = new GameObject[5];
+        roomCentre = new Vector3(0, 0, 0);
+        addDoors();
+        addWall();
+	}
+
+    private void addDoors(){
+        components[1] = Instantiate(
+            doorWall,
+            roomCentre,
+            Quaternion.Euler(0,-90,0)
+        ) as GameObject;
+    }
+	private void addWall(){
+        components[1] = Instantiate(
+            filledWall,
+            roomCentre,
+            Quaternion.Euler(0, 90, 0)
+        ) as GameObject;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
