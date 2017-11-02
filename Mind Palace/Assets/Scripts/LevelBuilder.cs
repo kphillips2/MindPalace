7687372de326e9dd18b6b1a80d70443a9a04e604
SomethingKL <rbrandt@zagmail.gonzaga.edu>
2017-11-01 19:27:45 -0700

    finished roombuilder

--- Mind Palace/Assets/Scripts/LevelBuilder.cs
+++ Mind Palace/Assets/Scripts/LevelBuilder.cs
@@ -3,33 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour {
-    public GameObject room;
+	public GameObject room;
 
-    private GameObject currentRoom;
-    private RoomBuilder roomBuilder;
+	private GameObject currentRoom;
+	//private RoomBuilder roomBuilder;
 
-    // Use this for initialization
-    void Start () {
-        roomBuilder = room.GetComponent<RoomBuilder>();
-        currentRoom = Instantiate(
-            room,
-            new Vector3(0, 0, 20),
-            Quaternion.Euler(0, 0, 0)
-        ) as GameObject;
-        roomBuilder.addDoors(new Vector3(0, 0, 20));
-        roomBuilder.addWalls(new Vector3(0, 0, 20));
+	// Use this for initialization
+	void Start () {
+		//roomBuilder = room.GetComponent<RoomBuilder>();
 
-        currentRoom = Instantiate(
-            room,
-            new Vector3(-20, 0, 0),
-            Quaternion.Euler(0, 0, 0)
-        ) as GameObject;
-        roomBuilder.addDoors(new Vector3(-20, 0, 0));
-        roomBuilder.addWalls(new Vector3(-20, 0, 0));
-    }
-	
+		Vector3 centre = new Vector3(0, 0, 0);
+		int[] walls = {1, 2, 1, 2};
+		room.GetComponent<RoomBuilder>().addWalls(
+			centre,
+			walls
+		);
+
+		centre = new Vector3(0, 0, 20);
+		addRoom(centre, walls);
+
+		centre = new Vector3(0, 0, -20);
+		addRoom(centre, walls);
+	}
+
+	private void addRoom(Vector3 roomCentre, int[] input){
+		currentRoom = Instantiate(
+			room,
+			roomCentre,
+			Quaternion.Euler(0, 0, 0)
+		) as GameObject;
+		currentRoom.GetComponent<RoomBuilder>().addWalls(
+			roomCentre,
+			input
+		);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
--- Mind Palace/Assets/Scripts/RoomBuilder.cs
+++ Mind Palace/Assets/Scripts/RoomBuilder.cs
@@ -3,28 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomBuilder : MonoBehaviour {
-    public GameObject floor;
-    public GameObject filledWall;
-    public GameObject doorWall;
+	public GameObject floor;
+	public GameObject filledWall;
+	public GameObject doorWall;
 
-    private GameObject component;
-	
-    // Use this for initialization
+	private GameObject component;
+
+	// Use this for initialization
 	void Start (){
-    }
+		doorWall.SetActive(false);
+		filledWall.SetActive(false);
+	}
 
-    public void addDoors(Vector3 centre){
-        component = Instantiate(
-            doorWall,
-            centre,
-            Quaternion.Euler(0,-90,0)
-        ) as GameObject;
-    }
-	public void addWalls(Vector3 centre){
-        component = Instantiate(
-            filledWall,
-            centre,
-            Quaternion.Euler(0, 90, 0)
-        ) as GameObject;
-    }
+	// input: Vector3 for the center of the room
+	//        byte array for the state of each wall
+	public void addWalls(Vector3 roomCentre, int[] input){
+		// Wall Locations are the indices of input
+
+		//       0
+		//    --------
+		//   |        |
+		// 3 |        |
+		//   |        | 1
+		//   |        |
+		//    --------
+		//       2
+
+		// wall with door is 1
+		// wall without door is 2
+
+		for(int k=0;k<4;k++)
+			switch(input[k]){
+				case 0:
+					break;
+				case 1:
+					addDoor(roomCentre, k*90);
+					break;
+				default:
+					addWall(roomCentre, (k-1)*90);
+					break;
+			}
+	}
+
+	// input: angle representing which side to put the wall on
+	//        0 degrees is wall location 0
+	private void addDoor(Vector3 roomCentre, int angle){
+		component = Instantiate(
+			doorWall,
+			roomCentre,
+			Quaternion.Euler(0, angle, 0)
+		) as GameObject;
+	}
+	// input: angle representing which side to put the wall on
+	//        -90 degrees is wall location 0
+	private void addWall(Vector3 roomCentre, int angle){
+		component = Instantiate(
+			filledWall,
+			roomCentre,
+			Quaternion.Euler(0, angle, 0)
+		) as GameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
 }
