5a8e24d9bdd8fc0747d1c0be1eccb9f4a9d5fed5
mbaker7@zagmail.gonzaga.edu <mbaker7@zagmail.gonzaga.edu>
2017-11-01 19:19:09 -0700

    pause menu

---
+++ Mind Palace/Assets/Scripts/TouchpadMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Written by: Max Baker
+ * Last Modified: 10/1/17
+ * 
+ * Purpose: Test run for implementing Vive movement via trackpad.
+ * 
+ * Usage: Drag to a Controller object in unity, then drag the [CameraRig] into the cameraRigTransform field.
+ */
+public class TouchpadMovement : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    public Transform cameraRigTransform;
+    private Vector3 viewDir;
+    private Vector3 normalViewDir;
+
+    private float height;
+    private SteamVR_Controller.Device device;
+    private float speed = 5.0f;
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetAxis().x != 0 || device.GetAxis().y != 0)
+        {
+            viewDir = SteamVR_Render.Top().GetRay().direction;
+            normalViewDir = new Vector3(viewDir.z, 0, -viewDir.x);
+            height = cameraRigTransform.position.y;
+            cameraRigTransform.position = cameraRigTransform.position + speed * Time.deltaTime * device.GetAxis().y * viewDir;
+            cameraRigTransform.position = cameraRigTransform.position + speed * Time.deltaTime * device.GetAxis().x * normalViewDir;
+            cameraRigTransform.position = new Vector3(cameraRigTransform.position.x, height, cameraRigTransform.position.z);
+        }
+    }
+}
---
+++ Mind Palace/Assets/Scripts/pauseListener.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using UnityEngine.SceneManagement;
+
+/*
+ * Name: Max Baker
+ * Date: 11/1/17
+ * Add this to a controller, give it the menu canvases
+ * Assign the methods to the corresponding menu button
+ * */
+
+public class pauseListener : MonoBehaviour {
+   
+    public GameObject pauseMenu; //pause canvas
+    public GameObject settingsMenu; //settings canvas
+    public VRTK_Pointer pointer; //TBD
+    public VRTK_StraightPointerRenderer poin; //TBD
+    public Transform cameraRigTransform; //camera rig
+
+    private Vector3 viewDir;
+    private float height = 1.5f;
+    private float distance = 5.0f;
+    private SteamVR_TrackedController controller;
+    private bool paused = false;
+
+    //adds listener to menu button
+    void OnEnable () {
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.MenuButtonClicked += MenuPress;
+    }
+
+    //reveals or hides menu and move it to infront of the user, plus rotates it to face the user
+    private void MenuPress(object sender, ClickedEventArgs e)
+    {
+        paused = pauseMenu.activeSelf;
+        paused = !paused;
+        pauseMenu.SetActive(paused);
+        settingsMenu.SetActive(false);
+        viewDir = SteamVR_Render.Top().GetRay().direction;
+        pauseMenu.transform.position = cameraRigTransform.position + distance*new Vector3(viewDir.normalized.x, height/distance,viewDir.normalized.z);
+        pauseMenu.transform.rotation = Quaternion.LookRotation(new Vector3(viewDir.x,0,viewDir.z));
+    }
+	
+    //Moves and rotates the settings canvas
+    public void GoToSettings()
+    {
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+        settingsMenu.transform.position = pauseMenu.transform.position;
+        settingsMenu.transform.rotation = pauseMenu.transform.rotation;
+    }
+
+    public void LeaveSettings()
+    {
+        pauseMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        paused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
