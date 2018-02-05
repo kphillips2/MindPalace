f0dd31dd931c1ec9997215b9e8daaabf3205d3a6
kphillips2 <kephillips@comcast.net>
2018-02-04 16:47:17 -0800

    Grabbable versions of objects added

---
+++ Mind Palace/Assets/Scripts/SnapToGround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attaching this script to an object will make it so that when the user grabs the object, the xz
+// coordinates will be updated but the object will remain stuck to the ground
+
+public class SnapToGround : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
+
+        if (transform.position.y != 0) {
+            //Rigidbody is added to object when it's picked up, needs to be removed or else object slides
+            Destroy(GetComponent<Rigidbody>());
+            transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+        }
+    }
+}
