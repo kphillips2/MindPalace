95c22751ad899dad588cdf9969b2d61105f5a003
Julie Prichard <jprichard@zagmail.gonzaga.edu>
2018-03-04 14:04:11 -0800

    For Max

--- Mind Palace/Assets/Scripts/FloorMenu.cs
+++ Mind Palace/Assets/Scripts/FloorMenu.cs
@@ -86,16 +86,58 @@ public class FloorMenu : MonoBehaviour {
         switch(ThemeNumber){
             case 1:
                 RoomTextureSetter.setMaterials(
-                "Wood Texture 06", // floor material
-                "Wood Texture 15", // roof material
+                "Wood Texture 15", // floor material
+                "Wood Texture 12", // roof material
                 "Wood texture 12"  // wall material
                  );
                 break;
             case 2:
                 RoomTextureSetter.setMaterials(
-                "Wood Texture 12", // floor material
-                "Wood Texture 06", // roof material
-                "Wood texture 15"  // wall material
+                "Wood Texture 01", // floor material
+                "Wood Texture 05", // roof material
+                "Wood texture 05"  // wall material
+                 );
+                break;
+            case 3:
+                RoomTextureSetter.setMaterials(
+                "Wood Texture 13", // floor material
+                "Wood Texture 12", // roof material
+                "Wood texture 12"  // wall material
+                 );
+                break;
+            case 4:
+                RoomTextureSetter.setMaterials(
+                "Wood Texture 14", // floor material
+                "Wood Texture 03", // roof material
+                "Wood texture 03"  // wall material
+                 );
+                break;
+            case 5:
+                RoomTextureSetter.setMaterials(
+                "Wood Texture 06", // floor material
+                "Wood Texture 04", // roof material
+                "Wood texture 04"  // wall material
+                 );
+                break;
+            case 6:
+                RoomTextureSetter.setMaterials(
+                "Wood Texture 15", // floor material
+                "Wood Texture 11", // roof material
+                "Wood texture 11"  // wall material
+                 );
+                break;
+            case 7:
+                RoomTextureSetter.setMaterials(
+                "Wood Texture 15", // floor material
+                "Wood Texture 09", // roof material
+                "Wood texture 09"  // wall material
+                 );
+                break;
+            case 8:
+                RoomTextureSetter.setMaterials(
+                "Wood Texture 02", // floor material
+                "Wood Texture 03", // roof material
+                "Wood texture 03"  // wall material
                  );
                 break;
             default:
