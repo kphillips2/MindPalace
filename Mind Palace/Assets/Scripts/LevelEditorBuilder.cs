56ff556e61d3e64590b73d5724ae1044477ae2c5
mbaker7@zagmail.gonzaga.edu <mbaker7@zagmail.gonzaga.edu>
2018-03-04 20:50:54 -0800

    CorridorsCrappyPlusSigns

--- Mind Palace/Assets/Scripts/LevelEditorBuilder.cs
+++ Mind Palace/Assets/Scripts/LevelEditorBuilder.cs
@@ -11,29 +11,14 @@ public class LevelEditorBuilder : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		//oldCorridor = corridor.GetComponent<OldCorridor> ();
         pictureCreator = new PictureCreator();
 		building = GetComponentInParent<Building> (); 
-		//roomCreator = room.GetComponent<RoomCreator> ();
-        /*
-		// input: index, loc (-6)------------(6)
-		roomCreator.addDoor (0, -3);
-		roomCreator.addDoor (1, 0);
-		//roomCreator.addDoor (2, -3);
-		roomCreator.addDoor (3, 3);
-        */
-
-		//roomCreator.setMaterials(
-		//	"Wood Texture 06", // floor material
-		//	"Wood Texture 15", // roof material
-		//	"Wood texture 12"  // wall material
-		//);
-        ////roomBuilder.addDoor(1, -3);
-        ////roomBuilder.addDoor(1, 3);
-        //GameObject FirstRoom =addRoom(new Vector3(0,0,0));
 
 		building.addRoom (new Vector3 (0, 0, 0));
-        
+        int[] DummyDoors = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        string[] DummyMat = { "", "", "" };
+        SaveLoad.currentLoci.addRoom(new Room(DummyDoors, new Vector3(0,0,0), DummyMat));
+
     }
 
 	public GameObject addRoom(Vector3 roomCentre){
@@ -46,9 +31,35 @@ public class LevelEditorBuilder : MonoBehaviour {
         return component;
     }
 
+    public GameObject addCorridorAlongZ(Vector3 roomCentre)
+    {
+        GameObject component = Instantiate(
+            room,
+            roomCentre,
+            Quaternion.Euler(0, 0, 0)
+        ) as GameObject;
+        component.SetActive(true);
+        component.GetComponent<RoomBuilder>().setRoomSize(4, 24);
+        return component;
 
-	// Update is called once per frame
-	void Update () {
+    }
+
+    public GameObject addCorridorAlongX(Vector3 roomCentre)
+    {
+        GameObject component = Instantiate(
+            room,
+            roomCentre,
+            Quaternion.Euler(0, 0, 0)
+        ) as GameObject;
+        component.SetActive(true);
+        component.GetComponent<RoomBuilder>().setRoomSize(24, 4);
+        return component;
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 
 	}
 }
