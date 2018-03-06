03684fae25e26a1f7fca5c1b492aa4f479aea705
Julie Prichard <jprichard@zagmail.gonzaga.edu>
2018-03-05 18:43:18 -0800

    Terrains

--- Mind Palace/Assets/Scripts/StartingScreen.cs
+++ Mind Palace/Assets/Scripts/StartingScreen.cs
@@ -4,36 +4,102 @@ using UnityEngine;
 
 public class StartingScreen : MonoBehaviour {
 
-    public GameObject room;
+    public OldRoom room;
     private PictureCreator pictureCreator;
-    private Building building;
-    private GameObject component;
-    private RoomBuilder roomTextureSetter;
+    private int[] roomDoors;
 
     // Use this for initialization
     void Start () {
 
         pictureCreator = new PictureCreator();
-        building = GetComponentInParent<Building>();
-        building.addRoom(new Vector3(0, 0, 0));
-        roomTextureSetter = room.GetComponent<RoomBuilder>();
-        roomTextureSetter.setMaterials(
+        roomDoors = new int[] { 0, 1, 0, 0 };
+        room.setMaterials(
                 "Wood Texture 15", // floor material
-                "Wood Texture 12", // roof material
-                "Wood texture 12"  // wall material
+                "Wood Texture 05", // roof material
+                "Wood texture 05"  // wall material
                  );
+        room.addFloor(new Vector3(0, 0, 0));
+        room.addRoof(new Vector3(0, 0, 0));
+        room.addWalls(
+            new Vector3(0, 0, 0),
+            roomDoors
+        );
+        //add pictures
+        //hangPictures(new Vector3(0, 0, 0), roomDoors);
     }
-
-    public GameObject addRoom(Vector3 roomCentre)
+    /*
+    private void hangPictures(Vector3 centre, int[] roomDoors)
     {
-        component = Instantiate(
-            room,
-            roomCentre,
-            Quaternion.Euler(0, 0, 0)
-        ) as GameObject;
-        component.SetActive(true);
-        return component;
+        string[] imageRes = { "Assets/Resources/treepic.jpg" , "Assets/Resources/tamarin.jpg", "Assets/Resources/seadragon.jpg",
+            "Assets/Resources/owl.jpg", "Assets/Resources/camel.JPG", "Assets/Resources/mountain.jpeg",
+            "Assets/Resources/shoreline.jpeg", "Assets/Resources/chalk.jpeg", "Assets/Resources/christmas.jpeg",
+            "Assets/Resources/glowsticks.jpeg", "Assets/Resources/donuts.jpeg", "Assets/Resources/sparkler.jpeg",
+            "Assets/Resources/valley.jpeg", "Assets/Resources/iceberg.jpeg", "Assets/Resources/space.jpeg",
+            "Assets/Resources/lamb.jpeg"};
+
+        int randPic = Random.RandomRange(0, imageRes.Length);
+
+        for (int i = 0; i < 4; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    if (roomDoors[0] == 1)
+                    {
+                        pictureCreator.placePicture(imageRes[randPic], 90f, centre + new Vector3(-(4.7f / 1.5f), 2.6f, 4.7f));
+                        pictureCreator.placePicture(imageRes[(randPic + 1) % imageRes.Length], 90f, centre + new Vector3((4.7f / 1.5f), 2.6f, 4.7f));
+                    }
+                    else
+                    {
+                        pictureCreator.placePicture(imageRes[(randPic + 0) % imageRes.Length], 90f, centre + new Vector3(-(4.7f / 1.5f), 2.6f, 4.7f));
+                        pictureCreator.placePicture(imageRes[(randPic + 1) % imageRes.Length], 90f, centre + new Vector3(0, 2.6f, 4.7f));
+                        pictureCreator.placePicture(imageRes[(randPic + 2) % imageRes.Length], 90f, centre + new Vector3((4.7f / 1.5f), 2.6f, 4.7f));
+                    }
+                    break;
+                case 1:
+                    if (roomDoors[1] == 1)
+                    {
+                        pictureCreator.placePicture(imageRes[(randPic + 3) % imageRes.Length], 180f, centre + new Vector3(4.7f, 2.6f, -(4.7f / 1.5f)));
+                        pictureCreator.placePicture(imageRes[(randPic + 4) % imageRes.Length], 180f, centre + new Vector3(4.7f, 2.6f, (4.7f / 1.5f)));
+                    }
+                    else
+                    {
+                        pictureCreator.placePicture(imageRes[(randPic + 3) % imageRes.Length], 180f, centre + new Vector3(4.7f, 2.6f, -(4.7f / 1.5f)));
+                        pictureCreator.placePicture(imageRes[(randPic + 4) % imageRes.Length], 180f, centre + new Vector3(4.7f, 2.6f, 0));
+                        pictureCreator.placePicture(imageRes[(randPic + 5) % imageRes.Length], 180f, centre + new Vector3(4.7f, 2.6f, (4.7f / 1.5f)));
+                    }
+                    break;
+                case 2:
+                    if (roomDoors[2] == 1)
+                    {
+                        pictureCreator.placePicture(imageRes[(randPic + 6) % imageRes.Length], -90f, centre + new Vector3((4.7f / 1.5f), 2.6f, -4.7f));
+                        pictureCreator.placePicture(imageRes[(randPic + 7) % imageRes.Length], -90f, centre + new Vector3(-(4.7f / 1.5f), 2.6f, -4.7f));
+                    }
+                    else
+                    {
+                        pictureCreator.placePicture(imageRes[(randPic + 6) % imageRes.Length], -90f, centre + new Vector3((4.7f / 1.5f), 2.6f, -4.7f));
+                        pictureCreator.placePicture(imageRes[(randPic + 7) % imageRes.Length], -90f, centre + new Vector3(0, 2.6f, -4.7f));
+                        pictureCreator.placePicture(imageRes[(randPic + 8) % imageRes.Length], -90f, centre + new Vector3(-(4.7f / 1.5f), 2.6f, -4.7f));
+                    }
+                    break;
+                case 3:
+                    if (roomDoors[3] == 1)
+                    {
+                        pictureCreator.placePicture(imageRes[(randPic + 9) % imageRes.Length], 0f, centre + new Vector3(-4.7f, 2.6f, (4.7f / 1.5f)));
+                        pictureCreator.placePicture(imageRes[(randPic + 10) % imageRes.Length], 0f, centre + new Vector3(-4.7f, 2.6f, -(4.7f / 1.5f)));
+                    }
+                    else
+                    {
+                        pictureCreator.placePicture(imageRes[(randPic + 9) % imageRes.Length], 0f, centre + new Vector3(-4.7f, 2.6f, (4.7f / 1.5f)));
+                        pictureCreator.placePicture(imageRes[(randPic + 10) % imageRes.Length], 0f, centre + new Vector3(-4.7f, 2.6f, 0f));
+                        pictureCreator.placePicture(imageRes[(randPic + 11) % imageRes.Length], 0f, centre + new Vector3(-4.7f, 2.6f, -(4.7f / 1.5f)));
+                    }
+                    break;
+            }
+        }
+        
     }
+*/
 
     // Update is called once per frame
     void Update () {
